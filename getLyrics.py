081b932db2f904164c18e5a35991525e0789957e
ta <unagiinu_japan@yahoo.co.jp>
2016-07-09 17:00:12 +0900

    test

--- getLyrics.py
+++ getLyrics.py
@@ -1,20 +1,41 @@
 #http://stackoverflow.com/questions/24873773/web-scraping-rap-lyrics-on-rap-genius-w-python
 
-from urlparse import urljoin
-from bs4 import BeautifulSoup
+import re
 import requests
+import nltk
+from bs4 import BeautifulSoup
+
+url = 'http://www.lyrics.com/eminem'
+r = requests.get(url)
+soup = BeautifulSoup(r.content)
+gdata = soup.find_all('div',{'class':'row'})
+
+eminemLyrics = []
+
+for item in gdata:
+    title = item.find_all('a',{'itemprop':'name'})[0].text
+    lyricsdotcom = 'http://www.lyrics.com'
+    for link in item('a'):
+        try:
+            lyriclink = lyricsdotcom+link.get('href')
+            req = requests.get(lyriclink)
+            lyricsoup = BeautifulSoup(req.content)
+            lyricdata = lyricsoup.find_all('div',{'id':re.compile('lyric_space|lyrics')})[0].text
+            eminemLyrics.append([title,lyricdata])
+            print(title)
+            print(lyricdata)
+            print()
+        except:
+            pass
 
 
-BASE_URL = "http://genius.com"
-artist_url = "http://genius.com/artists/Andre-3000/"
 
-response = requests.get(artist_url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36'})
 
-soup = BeautifulSoup(response.text, "lxml")
-for song_link in soup.select('ul.song_list > li > a'):
-    link = urljoin(BASE_URL, song_link['href'])
-    response = requests.get(link)
-    soup = BeautifulSoup(response.text)
-    lyrics = soup.find('div', class_='lyrics').text.strip()
+Titles = [i[0] for i in eminemLyrics]
+Lyrics = [i[1] for i in eminemLyrics]
 
-    # tokenize `lyrics` with nltk
+from sklearn.feature_extraction.text import CountVectorizer
+Vectorizer = CountVectorizer(min_df = 1)
+X = Vectorizer.fit_transform(Lyrics)
+X.toarray()
+Vectorizer.get_feature_names()
